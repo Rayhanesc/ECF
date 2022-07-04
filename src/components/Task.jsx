873862cd8ae8c89330693b010d6cad5553eb4912
Rayhanesc <105655614+Rayhanesc@users.noreply.github.com>
2022-07-04 16:59:41 +0200

    dd

--- src/components/Task.jsx
+++ src/components/Task.jsx
@@ -2,7 +2,7 @@ const Task = ({ todo }) => {
   return (
     <div
      
-      style={{ textAlign: "center", padding: 20 ,border: "0px solid white",}}
+      style={{ textAlign: "center", padding: 20 }}
     >
       {todo.title}
       <input className="input" textAlign="left" type="checkbox" id="horns" name="horns" />
